73533a0f5762f9d872b6664dc03f386c318d46b4
rq-NRiver520-at-321899771753 <guwa.new.game@gmail.com>
2024-09-25 11:48:52 +0900

    ログイン機能/ユーザー登録

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@yourapp.com'
+
+  def registration_confirmation(user)
+    @user = user
+    @confirmation_url = confirm_email_url(token: @user.confirmation_token) # 確認用URLを生成
+    mail(to: @user.email, subject: '会員登録の確認')
+  end
+end
