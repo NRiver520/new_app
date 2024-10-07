85040db79154cb52329992f250dc2da0393e5827
rq-NRiver520-at-321899771753 <guwa.new.game@gmail.com>
2024-10-07 14:50:41 +0900

    Add Gmail environment variables for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,5 +100,15 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.default_url_options = { host: 'nameless-depths-58171-a7eccb1f4a50.herokuapp.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'nameless-depths-58171-a7eccb1f4a50.herokuapp.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
 end
