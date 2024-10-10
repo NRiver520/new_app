4b6d31125c4a4453109ddf5aa4aa5a115f95c114
rq-NRiver520-at-321899771753 <guwa.new.game@gmail.com>
2024-10-10 20:16:04 +0900

    サイトに説明記載

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,21 @@
 module ApplicationHelper
-
-  def page_title(title = '')
-    base_title = 'ベイブレード交流掲示板'
+  def page_title(title = "")
+    base_title = "ベイブレード交流掲示板"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
 
   def rank_border_class(rank)
     case rank
-    when 'ノーランク'
-      'border-light'
-    when 'ブロンズ'
+    when "ノーランク"
+      "border-light"
+    when "ブロンズ"
       'border-primary'
-    when 'シルバー'
-      'border-success'
-    when 'ゴールド'
-      'border-danger'
-    when 'プラチナ'
-      'border-warning'
+    when "シルバー"
+      "border-success"
+    when "ゴールド"
+      "border-danger"
+    when "プラチナ"
+      "border-warning"
     end
   end
 end
