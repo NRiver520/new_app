5a505b28570ee61d80ef7e6037962a2a90b75f3b
rq-NRiver520-at-321899771753 <guwa.new.game@gmail.com>
2024-10-10 20:46:19 +0900

    サイトに説明記載

--- db/migrate/20240930044721_create_comments.rb
+++ db/migrate/20240930044721_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.2]
     create_table :comments do |t|
       t.references :user, foreign_key: true
       t.references :board, foreign_key: true
-      t.text :body    
+      t.text :body
       t.timestamps
     end
   end
