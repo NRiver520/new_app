332fe7766cca27ae00f9a020d6bdff4e7512ba91
rq-NRiver520-at-321899771753 <guwa.new.game@gmail.com>
2024-11-13 19:41:13 +0900

    デザイン修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,11 +5,11 @@ class BoardsController < ApplicationController
   def index
     @q = Board.ransack(params[:q])
     sort_order = case params[:sort]
-                 when "newest" then { created_at: :desc }
-                 when "oldest" then { created_at: :asc}
-                 when "most_commented" then "COUNT(comments.id) DESC"
-                 else { created_at: :desc }
-                 end
+                  when "newest" then { created_at: :desc }
+                  when "oldest" then { created_at: :asc }
+                  when "most_commented" then "COUNT(comments.id) DESC"
+                  else { created_at: :desc }
+                end
     @boards = @q.result(distinct: true)
                 .includes(:user)
                 .left_joins(:comments)
